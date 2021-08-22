b0f43a2e7a1a0e0875567fd8abb60971da01d9c6
Leonardo Henrique Guimarães <leonardo.guimaraeslhg@gmail.com>
2021-08-22 14:37:26 -0300

    Implementing User Class

---
+++ projeto/app/src/main/java/src/model/User.java
@@ -0,0 +1,36 @@
+public class User{
+    public User(){}
+
+    private int ID;
+    private String Name;
+    private String Password;
+    private String Address;
+    
+    public int getID(){
+        return ID;
+    }
+
+    public void setName(String Name){
+        this.Name = Name;
+    }
+
+    public String getName(){
+        return Name;
+    }
+
+    public void setPassword(String Password){
+        this.Password = Password;
+    }
+
+    public String getPassword(){
+        return Password;
+    }
+
+    public void setAddress(String Address){
+        this.Address = Address;
+    }
+
+    public String getAddress(){
+        return Address;
+    }
+}
