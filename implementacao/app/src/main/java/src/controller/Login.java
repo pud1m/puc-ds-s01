8cecb0a3b718d059c370b183d69b8546650ca5e7
Leonardo Henrique Guimarães <leonardo.guimaraeslhg@gmail.com>
2021-08-22 16:15:39 -0300

    Fixing project under wrong name

---
+++ implementacao/app/src/main/java/src/controller/Login.java
@@ -0,0 +1,11 @@
+public class Login{
+    public Login(){}
+
+    private void getName(){}
+
+    private void getPassword(){}
+
+    public void getUser(){}
+    
+    public void validateLogin(){}
+}
