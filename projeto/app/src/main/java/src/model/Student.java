1030ba2af9f46cce3c183e60444538a98c26b83d
Leonardo Henrique Guimarães <leonardo.guimaraeslhg@gmail.com>
2021-08-22 15:15:50 -0300

    Implementing Student Class

---
+++ projeto/app/src/main/java/src/model/Student.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Student extends User{
+    public Student(){}
+
+    private Registration registrations[];
+    private Course course;
+
+    public void createRegistrations(String Discipline){
+        //
+    }
+
+    public void cancelRegistrations(String Discipline){
+        //
+    }
+} 
